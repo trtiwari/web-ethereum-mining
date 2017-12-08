da4df448c9cc08d4e102f248b66206f8f3c6d632
djfinn14 <djfinn14@gmail.com>
2017-12-08 09:00:44 -0500

    Modified helper.js to change the python to JS

--- helper.js
+++ helper.js
@@ -3,27 +3,38 @@ var FNV_PRIME = 0x01000193;
 function encode_int(s)
 {
 	// convert format string to js equivalents
-	a = "%x" % s
+	//a = "%x" % s
+	a = s.toString(16)
 	if (s == 0)
 	{
 		return '';
 	}
 	else
-	{
-		return ('0' * (a.length % 2) + a).decode('hex')[::-1];
+	{	
+		hex = Array(a.length % 2 + 1).join('0') + a
+		for (var i = 0; i < hex.length; i += 2) str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+		//return ('0' * (a.length % 2) + a).decode('hex')[::-1];
+		return str.split("").reverse().join("")
 	}
 }
 
 // convert to js equivalents
 function zpad(s, length)
 {
-    return s + '\x00' * Math.max(0, length - s.length);
+    //return s + '\x00' * Math.max(0, length - s.length);
+    return s + Array(Math.max(0, length - s.length) + 1).join('\x00') 
 }
 
 // convert to js equivalents
 function serialize_hash(h)
 {
-    return ''.join([zpad(encode_int(x), 4) for x in h]);
+    str = ""
+    for (x in h)
+    {
+	str += 	zpad(encode_int(x), 4)
+    }
+    //return ''.join([zpad(encode_int(x), 4) for x in h]);
+    return str
 }
 
 // convert to js equivalents
