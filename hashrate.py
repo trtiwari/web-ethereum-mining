78ce033ce9f0520575838e0e694925452abe0ca7
root <root@localhost.localdomain>
2017-12-08 15:55:43 -0500

    Added the python script to measure hashrate for geth

---
+++ hashrate.py
@@ -0,0 +1,16 @@
+#use the following commands
+#dependant on web3.py, npm install web3
+#./geth --identity "TestNet" --nodiscover --networkid 700 --datadir ~/ethblock --rpc --rpcaddr localhost --rpcport 4000 --mine console
+
+from web3 import Web3, HTTPProvider, IPCProvider
+import time
+
+web3 = Web3(HTTPProvider('http://localhost:4000'))
+t_end = time.time() + 60 * 1
+f = open('test.txt','w')
+old = 0
+while time.time() < t_end:
+	f.write(str(web3.eth.hashrate))
+	print (str(web3.eth.hashrate))
+	time.sleep(1)
+f.close()
