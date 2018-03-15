9a3cc58e8b195926f658baedd85a19f195ac23a1
Trishita <trtiwari@bu.edu>
2018-03-14 23:51:31 -0400

    emperically measured distribution of DAG accesses -- its uniform :(

---
+++ paper/dag-distribution.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import pylab as plt
+import numpy as np
+
+def main():
+	with open("distribution-data.txt") as handler:
+		lines = handler.read().splitlines()[1:]
+		data = np.array([int(i) for i in lines])
+		# plt.barh(np.arange(len(data)),data,align='center')
+		print sum(data[:len(data)/3])/float(len(data)/3)
+		print sum(data[len(data)/3:2*len(data)/3])/float(2*len(data)/3 - len(data)/3)
+		print sum(data[2*len(data)/3:])/float(len(data) - 2*len(data)/3)
+		# plt.plot(data,'r-')
+		# plt.show()
+
+if __name__ == '__main__':
+	main()
