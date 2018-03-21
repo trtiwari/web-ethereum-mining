0fd2205639fcc18267b969d920ae54d53eb15bb5
Trishita Tiwari <trtiwari@bu.edu>
2018-03-21 03:43:16 -0400

    more res

--- webassembly/results/native-miner.py
+++ webassembly/results/native-miner.py
@@ -3,3 +3,36 @@
 hash_rate = dict()
 cache_hit_rate = dict()
 
+hash_rate[1] = 
+cache_hit_rate[1] = 
+
+hash_rate[0.9] = 
+cache_hit_rate[0.9] = 
+
+hash_rate[0.8] = 
+cache_hit_rate[0.8] = 
+
+hash_rate[0.7] = 
+cache_hit_rate[0.7] = 
+
+hash_rate[0.6] = 
+cache_hit_rate[0.6] = 
+
+hash_rate[0.5] = 
+cache_hit_rate[0.5] = 
+
+hash_rate[0.4] = 
+cache_hit_rate[0.4] =
+
+hash_rate[0.4] = 
+cache_hit_rate[0.4] =
+
+hash_rate[0.3] = 
+cache_hit_rate[0.3] =
+
+hash_rate[0.2] = 
+cache_hit_rate[0.2] =
+
+hash_rate[0.1] = 
+cache_hit_rate[0.1] = 
+
